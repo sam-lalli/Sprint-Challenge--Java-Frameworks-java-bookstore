2df5cfd76882b050df7edcae36532d258a78b4fc
Sam Lalli <2sam.lalli@gmail.com>
2020-11-13 11:11:02 -0700

    tests work, ready to be deployed

--- bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
@@ -2,17 +2,30 @@ package com.lambdaschool.bookstore.services;
 
 import com.lambdaschool.bookstore.BookstoreApplication;
 import com.lambdaschool.bookstore.exceptions.ResourceNotFoundException;
+import com.lambdaschool.bookstore.models.Author;
+import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.Section;
+import com.lambdaschool.bookstore.models.Wrote;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookstoreApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 //**********
 // Note security is handled at the controller, hence we do not need to worry about security here!
 //**********
@@ -27,6 +40,11 @@ public class BookServiceImplTest
             Exception
     {
         MockitoAnnotations.initMocks(this);
+
+        List<Book> myList = bookService.findAll();
+        for (Book b : myList) {
+            System.out.println(b.getBookid() + " " + b.getTitle());
+        }
     }
 
     @After
@@ -38,27 +56,48 @@ public class BookServiceImplTest
     @Test
     public void findAll()
     {
-        
+        assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void findBookById()
     {
+        assertEquals("Digital Fortess", bookService.findBookById(27).getTitle());
     }
 
     @Test(expected = ResourceNotFoundException.class)
     public void notFindBookById()
     {
+        assertEquals(null, bookService.findBookById(100).getTitle());
     }
 
     @Test
-    public void delete()
+    public void z_delete()
     {
+        bookService.delete(30);
+        assertEquals(4, bookService.findAll().size());
+        //should return 4 since book with the id of 30 is deleted
     }
 
     @Test
-    public void save()
+    public void z_save()
     {
+        Author a1 = new Author("John", "Mitchell");
+        a1.setAuthorid(101);
+
+        Section s1 = new Section();
+        s1.setSectionid(100);
+        s1.setName("Non-Fiction");
+
+//        Set<Wrote> wrote = new HashSet<>();
+        Book b3 = new Book("Test Book", "9780307474278", 2009, s1);
+        b3.getWrotes().add(new Wrote(a1, new Book()));
+//        b3.setWrotes(wrote);
+
+        Book addBook = bookService.save(b3);
+
+        assertNotNull(addBook);
+        assertEquals("Test Book", addBook.getTitle());
     }
 
     @Test
